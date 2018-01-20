9dfd9694ab6e34cf700a1e69d4e9c491e4ec82a0
4ked <max.goeke0@gmail.com>
2018-01-19 20:13:04 -0600

    form and console vals

---
+++ Analysis/form.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	
+	var button = document.getElementById('manualScout');
+	button.addEventListener("click", function () {
+		var scoutName = document.getElementById('scoutName').value;
+		var matchNum  = document.getElementById('matchNum').value;
+		var teamNum	  = document.getElementById('teamNum').value;
+		
+		var blocks = {};
+		blocks.redSwitch	=	$("#redSwitch-count").val();
+		blocks.scale		=	$("#scale-count").val();
+		blocks.blueSwitch	=	$("#blueSwitch-count").val();
+		blocks.levitate		=	$("#levitate-count").val();
+		blocks.force		=	$("#force-count").val();
+		blocks.boost		=	$("#boost-count").val();
+		
+		var climbMech 	= document.getElementById('climbMech').value;
+		var winLoss	 	= document.getElementById('winLoss').value;
+		
+		console.log("Scouter Name: " + scoutName);
+		console.log("Match Number: " + matchNum);
+		console.log("Team Number: " + teamNum);
+		console.log(blocks);
+		console.log("Climbing Mech: " + climbMech);
+		console.log("Win or Loss: " + winLoss);
+	});
+});
+
