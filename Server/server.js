ecc463f8289e430b99911a8583c3f9bf71ae4251
4ked <max.goeke0@gmail.com>
2018-02-21 17:16:48 -0600

    running example route

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,15 @@
 var express 			= require('express');
 var expressValidator 	= require('express-validator');
-
+var http				= require('http');
 var mongo      			= require('mongodb').MongoClient;
 var assert				= require('assert');
-
-var socketio 			= require('socket.io')();
+//var socketio 			= require('socket.io')();
 
 // web server
 var app = express();
 var route = express.Router(); // add support for express routing
 var server = http.Server(app);
-var io = socketio(server);
+//var io = socketio(server);
 
 // error handling middleware
 var errorHandler = function(err, req, res, next) {
@@ -24,4 +23,33 @@ var errorHandler = function(err, req, res, next) {
 };
 
 app.use(errorHandler);
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(3000, () => logg('Example app listening on port 3000!'));
+
+// logger that prevents circular object reference in javascript
+var logg = function(msg, obj) {
+    console.log('\n');
+    if(obj) {
+        try {
+            console.log(msg + JSON.stringify(obj));
+        } catch(err) {
+            var simpleObject = {};
+            for (var prop in obj ){
+                if (!obj.hasOwnProperty(prop)){
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'object'){
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'function'){
+                    continue;
+                }
+                simpleObject[prop] = obj[prop];
+            }
+            console.log('circular-' + msg + JSON.stringify(simpleObject)); // returns cleaned up JSON
+        }        
+    } else {
+        console.log(msg);
+    }
+};
 
