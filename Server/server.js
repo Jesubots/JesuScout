a7b6ad3c709d9dcf585e7353667f20ce97e5effb
4ked <max.goeke0@gmail.com>
2018-02-25 21:33:52 -0600

    adding mongo ops

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ var expressValidator 	= require('express-validator');
 //var request				= require('request'); // http trafficer
 var http				= require('http'); // http protocol
 var mongodb      		= require('mongodb'); // MongoDB driver
+var assert				= require('assert'); // mongo
 var bodyParser			= require('body-parser'); // http body parser
 var assert				= require('assert');
 var socketio 			= require('socket.io')();
@@ -11,21 +12,84 @@ var socketio 			= require('socket.io')();
 var mongo 				= mongodb.MongoClient;
 var objectID			= mongodb.ObjectID;
 
+const MONGO_URL = 'mongodb://localhost:27017';
+const DB_NAME   = 'jesuscout';
+
+/**
+ * MongoDB operations
+ * connects to MongoDB and registers a series of asynchronous methods
+ */
+Mongo.connect(MONGO_URL, function(err, client) {
+  
+    logg('what is err? ', err);
+  
+    const db = client.db(DB_NAME);
+
+    Mongo.ops = {};
+    
+    Mongo.ops.find = function(collection, json, callback) {
+        db.collection(collection).find(json).toArray(function(error, docs) {
+            if(callback) callback(error, docs);
+        });
+    };
+    
+    Mongo.ops.findOne = function(collection, json, callback) {
+        db.collection(collection).findOne(json, function(error, doc) {
+            if(callback) callback(error, doc);
+        });
+    };
+
+    Mongo.ops.insert = function(collection, json, callback) {
+        db.collection(collection).insert(json, function(error, result) {
+            if(callback) callback(error, result);
+        });
+    };
+
+    Mongo.ops.upsert = function(collection, query, json, callback) {
+        db.collection(collection).updateOne(query, { $set: json }, { upsert: true }, function(error, result) {
+            if (callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.updateOne = function(collection, query, json, callback) {
+        db.collection(collection).updateOne(query, { $set : json }, function(error, result) {
+            if(callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.deleteOne = function(collection, query, callback) {
+        db.collection(collection).deleteOne(query, function(error, result) {
+            if(callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.deleteMany = function(collection, query, callback) {
+        db.collection(collection).deleteMany(query, function(error, result) {
+            if(callback) callback(error, result);
+        });
+    };
+});
+
 var app = express();
 var route = express.Router(); // add support for express routing
 var server = http.Server(app);
 //var io = socketio(server);
 
 app.use(express.static('./../'));
-app.use(express.static('./../Assets/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(errorHandler);
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
-app.listen(3000, () => logg('JesuScout listening on port 3000!'));
+app.listen(3000, function() {
+	logg('listening on port 3000');
+});
 
-// body parsing ensures req.body property
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 // error handling middleware
 var errorHandler = function(err, req, res, next) {
@@ -37,8 +101,6 @@ var errorHandler = function(err, req, res, next) {
     res.render('error', { error : err });
     next(err);
 };
-
-app.use(errorHandler);
 
 /**
  * Middleware:
@@ -87,4 +149,3 @@ var logg = function(msg, obj) {
         console.log(msg);
     }
 };
-
